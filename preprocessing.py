9b112950acbce210e307ca4df72fa4bd8cf99197
Felix Hampel <felix.hampel@web.de>
2019-10-29 16:39:17 +0100

    convert dataset to csv WIP

--- preprocessing.py
+++ preprocessing.py
@@ -2,5 +2,29 @@
 Take the given dataset and bring in a form suited for our model
 """
 
-# TODO: get dataset and format it (probably according to the process described here:
-#  https://towardsdatascience.com/how-to-implement-seq2seq-lstm-model-in-keras-shortcutnlp-6f355f3e5639)
+import os
+import pandas as pd
+
+
+def to_csv(path):
+    data = {
+        'input': [],
+        'output': []
+    }
+    cur = 'input'
+    with open(path, encoding='utf-32') as f:
+        for line in f:
+            if line == '\n':
+                cur = 'input'
+                continue
+            else:
+                data[cur].append(line.rstrip('\n'))
+                cur = 'output'
+
+    df = pd.DataFrame(data)
+    df.to_csv(path_or_buf=os.path.join(os.getcwd(), 'data', 'smallest.csv'), index=False, encoding='utf-32')
+
+
+if __name__ == '__main__':
+    p = os.path.join(os.getcwd(), 'data', 'smallest.txt')
+    to_csv(p)
